0545d56f2bf32e33bd0b0d96cd5e13be41213aa9
EllieAzaveda <elliemiller66@icloud.com>
2021-02-06 23:09:27 -0700

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,51 @@
-function nameMenuItem() {
-
+// nameMenuItem
+function nameMenuItem(menuItem) {
+  return "Delicious " + menuItem;
 }
 
+//createMenuItem
+function createMenuItem(x, y, z) {
+  var menuItem = {
+    name: x,
+    price: y,
+    type: z,
+  }
+  return menuItem;
+}
+
+//addIngredients
+function addIngredients(ingName, ingredients) {
+if (ingredients.includes(ingName) === false) {
+  ingredients.push(ingName);
+}
+  return ingredients.length;
+}
+
+//formatPrice
+function formatPrice(formattedPrice, price) {
+  return "$" + formattedPrice;
+}
+
+//decreasePrice
+function decreasePrice(decreasedPrice, price) {
+  return decreasedPrice * 0.9;
+}
+
+//createRecipe
+function createRecipe(a, b, c) {
+  var recipe = {
+    title: a,
+    ingredients: b,
+    type: c,
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,6 +1,30 @@
 
+//takeOrder
+
+function takeOrder(newOrders, deliveryOrders) {
+  var newOrders;
+
+  if (newOrders.status === "accepted") {
+    deliveryOrders.slice(newOrders);
+  }
+
+  for(i = 0; i < deliveryOrders.length; i++) {
+    return deliveryOrders[i];
+  }
+}
+
+  // refundOrder
+
+
+  // listItems
+
+
+  // searchOrder
+
+
+
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
