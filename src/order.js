147ff0c579ef89fbe5f2347ee257289f8709928a
EllieAzaveda <elliemiller66@icloud.com>
2021-02-07 19:29:45 -0700

    Update project pt2 & some of pt3

--- src/order.js
+++ src/order.js
@@ -5,13 +5,22 @@ function takeOrder(newOrders, deliveryOrders) {
   }
 }
 
-function refundOrder() {
-  
+function refundOrder(newOrders, deliveryOrders) {
+  deliveryOrders.splice(newOrders - 1, 1);
 }
+
+function listItems(deliveryOrders) {
+
+}
+
+// function searchOrder(deliveryOrders, item) {
+//   deliveryOrders.find(item);
+// }
+
 
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
+  listItems,
   // searchOrder
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,12 +1,25 @@
 
-function createRestaurant(){
 
+function createRestaurant(restaurantName) {
+  restaurantName = {
+    name: restaurantName,
+      menus: {
+        breakfast: [],
+        lunch: [],
+        dinner: [],
+      }
+  }
+  return restaurantName;
+};
+
+function addMenuItem(restaurantName, menuItem) {
+  var chooseMenu = menuItem.type;
+  restaurantName.menus[chooseMenu].push(menuItem);
 }
 
 
-
 module.exports = {
-  // createRestaurant,
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
